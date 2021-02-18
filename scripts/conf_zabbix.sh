566efc950b5ca7a7aac1ed3823b2513e27a99a8a
vonKloppen <57313920+vonKloppen@users.noreply.github.com>
2021-02-18 18:32:45 +0100

    Delete scripts directory

--- scripts/conf_zabbix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i 's/Hostname=Zabbix\ server/Hostname='$(hostname)'/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/Server=127.0.0.1/Server=ZABBIX_SERVER/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/ServerActive=127.0.0.1/ServerActive=ZABBIX_SERVER:11051/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/LogFileSize=0/LogFileSize=10/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/#\ ListenPort=10050/ListenPort=11050/' /etc/zabbix/zabbix_agentd.conf
-
